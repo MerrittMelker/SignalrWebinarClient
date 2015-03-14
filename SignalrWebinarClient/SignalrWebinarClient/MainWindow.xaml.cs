75a9e45cb82d31f374dfba9cf80b0a8c2cb1a0b4
MerrittMelker <merrittmelker@gmail.com>
2015-03-13 19:40:42 -0500

    All the custom code to get the wpf working

---
+++ SignalrWebinarClient/SignalrWebinarClient/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Threading;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Transports;
+
+namespace SignalrWebinarClient
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupWidget();
+        }
+
+        private async void SetupWidget()
+        {
+            var hubConnection = new HubConnection("http://localhost:56531/");
+            var hubProxy = hubConnection.CreateHubProxy("widgetHub");
+
+            hubProxy.On<int>("updateWidgetCount",
+                count =>
+                    Dispatcher.BeginInvoke(DispatcherPriority.Normal, (Action) (() =>
+                    {
+                        WidgetCountTextBlock.Text = count.ToString();
+                    })));
+
+            await hubConnection.Start();
+        }
+    }
+}
